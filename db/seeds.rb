84125c00f46141c86411eaf7faae114baf672686
judas iscariot <reckmobile@gmail.com>
2015-03-03 21:19:23 +0000

    Complete seed-data-assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,40 @@
 require 'faker'
  
  # Create Posts
- 50.times do
+ 5.times do
    Post.create!(
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
  end
+
+if (Post.find_by title: 'Another idiotic Post') == nil
+ Post.create!(
+  title: "Another idiotic Post",
+  body: "judas is traitor!"
+   )
+end 
+
+
+
+
+
  posts = Post.all
  
  # Create Comments
- 100.times do
+ 10.times do
    Comment.create!(
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
  end
+
+ if (Comment.find_by body: "Dont be a hero!") == nil
+ Comment.create!(
+  #post: post,
+  body: "Dont be a hero!"
+   )
+end
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
