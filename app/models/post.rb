72f9132ea50f6f1c340e1397fb8a161db8b560a6
judas iscariot <reckmobile@gmail.com>
2015-04-01 22:04:25 +0100

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, lambda { order('title DESC') }
+  scope :ordered_by_reverse_created_at, lambda { order('created_at ASC') }
 end
