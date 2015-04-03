747d5498f30006e4c4264831042b3af01bf8499f
judas iscariot <reckmobile@gmail.com>
2015-04-03 18:09:45 +0100

    pundit authprization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ skip_before_action :flash_attack, only: [:index, :new]
 
  def index
     @posts = Post.all
+    authorize @posts
   end
 
   def show
@@ -14,12 +15,23 @@ skip_before_action :flash_attack, only: [:index, :new]
 
   def new
     @post = Post.new
+    authorize @post
   end
 
   def create
+    authorize @post
   @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+  authorize @post
+       if @post.save
+       flash[:notice] = "Post was saved."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :new
+     end
   end
   def update
+    authorize @post
     @post = Post.find(params[:id])
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
@@ -30,6 +42,7 @@ skip_before_action :flash_attack, only: [:index, :new]
     end
   end
   def edit
+    authorize @post
     @post = Post.find(params[:id])
   end
 end
