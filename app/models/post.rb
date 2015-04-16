d5ae9754ae7d728199111978614e917a7bc3ae30
judas iscariot <reckmobile@gmail.com>
2015-04-16 09:36:10 +0100

    checkpoint 44 rsepc mocking

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
 
   def up_votes
@@ -29,5 +29,9 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
  
     update_attribute(:rank, new_rank)
+  end
+
+  def create_vote
+  user.votes.create(value: 1, post: self)
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,17 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
+
+  include TestFactories
   
-        describe "validations" do
-        describe "value validation" do
+  
+    describe "validations" do
+      describe "value validation" do
         it "only allows -1 or 1 as values" do
-        @vote = Vote.new
-        if @vote.value == 1 or @vote.value == -1
-          expect( @vote.save ).to eq(true)
-        else
-          expect( @vote.save ).to eq(false)
-        end
-      end
+          up_vote = Vote.new(value: 1)
+          expect(up_vote.valid?).to eq(true)
+
+          down_vote = Vote.new(value: -1)
+          expect(down_vote.valid?).to eq(true)
+
+          invalid_vote = Vote.new(value: 2)
+          expect(invalid_vote.valid?).to eq(false)
+       end
     end
   end
+
+  describe 'after_save' do
+     it "calls `Post#update_rank` after save" do
+       post = associated_post
+       vote = Vote.new(value: 1, post: post)
+       expect(post).to receive(:update_rank)
+       vote.save
+     end
+   end
 end
+
