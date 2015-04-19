8541947022629520c4f631a5baef3d9b2942adda
judas iscariot <reckmobile@gmail.com>
2015-04-19 15:32:47 +0100

    favoriting-checkpoint-assignment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,26 @@ require 'rails_helper'
   describe User do
  
     include TestFactories
- 
+
     describe "#favorited(post)" do
-      xit "returns `nil` if the user has not favorited the post" do
+      
+      before do
+        @user = authenticated_user
+        @post = associated_post
+        @post2 = associated_post(title: "another title")
+        #favorite = @post.favorites.create( post: @post, user: @user )
+      end
+
+      it "returns `nil` if the user has not favorited the post" do
+        if @user.favorites.where(post_id: @post.id).first
+        favorite.update_attributes(user: @user, post: @post2)
+        end
+        expect( @user.favorited(@post) ).to eq(nil)
       end
  
-      xit "returns the appropriate favorite if it exists" do
+      it "returns the appropriate favorite if it exists" do
+        favorite = @user.favorites.create(post: @post, user: @user)
+        expect( @user.favorited(@post) ).to eql(favorite)
       end
     end
   end
